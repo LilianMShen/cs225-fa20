9bfc8812f89302bd671b2fd33a9286ae0915f004
Daniel Bargon <dbargon2@illinois.edu>
2020-12-05 15:16:21 -0600

    added catch header, basic test and data

---
+++ tests/testfileio.cpp
@@ -0,0 +1,13 @@
+#include "fileio.h"
+#include "catch/catch_amalgamated.hpp"
+#include <vector>
+using namespace fileio
+
+TEST_CASE("test_basic_fileio", "[fileio]") {
+    std::vector<std::string> basic = file_to_vector("data/basic.txt");
+    REQUIRE(basic[0] == "this");
+    REQUIRE(basic[1] == "is");
+    REQUIRE(basic[2] == "a");
+    REQUIRE(basic[3] == "test");
+}
+
