245072418a08a59342bb06991a80801882acd459
Justin Kim <justink6@linux-a1.ews.illinois.edu>
2020-12-05 20:28:06 -0600

    Added graph field + constructor/method outlines for landmark

--- landmark.h
+++ landmark.h
@@ -1,6 +1,16 @@
 #pragma once
 
+#include <vector>
+
 #include "BFS.h"
 #include "graph.h"
 #include "edge.h"
 
+class Landmark {
+  public: 
+    Landmark(std::vector<std::vector<std::string>> data);
+    std::vector<Edge> runLandmarkPath(Vertex start, Vertex end, Vertex landmark);
+
+  private:
+    Graph g_;
+}
