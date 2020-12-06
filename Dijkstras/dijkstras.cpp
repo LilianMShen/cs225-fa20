f57fab1bd52f0eaaacec82eb98887deed2b596f3
Spenser Fong <sfong5@linux-07.ews.illinois.edu>
2020-12-05 19:55:24 -0600

    Implemented graph constructor, untested

--- Dijkstras/dijkstras.cpp
+++ Dijkstras/dijkstras.cpp
@@ -1,2 +1,23 @@
+//#include <sstream>
+//#include <map>
+
 #include "dijkstras.h"
 #include "graph.h"
+
+Dijkstras::Dijkstras(vector<vector<string>> data) : g_(false, false) {
+    for (int i = 0; i < data.size() - 1; ++i) {
+        if (g_.vertexExists(data[i][0]) == false) {
+            g_.insertVertex(data[i][0]);
+        }
+        if (g_.vertexExists(data[i][1]) == false) {
+            g_.insertVertex(data[i][1]);
+        }
+        if (g_.edgeExists(data[i][0], data[i][1]) == false) {
+            g_.insertEdge(data[i][0], data[i][1]);
+        }
+    }
+}
+
+std::vector<Edge> Dijkstras::runDijkstras(Vertex a, Vertex b) {
+    
+}
--- Dijkstras/dijkstras.h
+++ Dijkstras/dijkstras.h
@@ -2,6 +2,18 @@
 
 #include <vector>
 #include <algorithm>
+#include <string>
 
 #include "graph.h"
 #include "edge.h"
+
+class Dijkstras {
+    public:
+        //Constructor that creates a graph from the dataset of connected users
+        Dijkstras(std::vector<std::vector<std::string>> data);
+
+        //Returns a vector of edges that represents the shortest path from Vertex a to b
+        std::vector<Edge> runDijkstras(Vertex a, Vertex b);
+    private:
+        Graph g_;
+}
