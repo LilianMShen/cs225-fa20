266f8b65adcfd7d4a8a98fb4c30371ab5938b3ac
Daniel Bargon <dbargon2@illinois.edu>
2020-12-07 17:50:09 -0600

    added tokenization tests

--- fileio.h
+++ fileio.h
@@ -9,7 +9,7 @@ namespace fileio {
 
 std::vector<std::string> file_to_vector(const std::string & file);
 
-std::vector<std::vector<std::string>> string_to_tokens(const std::vector<std::string> & csv);
+std::vector<std::vector<std::string>> csv_to_tokens(const std::vector<std::string> & csv);
 
 std::vector<std::string> split_string(std::string& to_split, const char& delim);
 
--- tests/testfileio.cpp
+++ tests/testfileio.cpp
@@ -11,3 +11,22 @@ TEST_CASE("test_basic_fileio", "[fileio]") {
     REQUIRE(basic[3] == "test");
 }
 
+TEST_CASE("test_basic_tokenization", "[fileio]") {
+    std::vector<std::string> lines = file_to_vector("tests/data/commas.txt");
+    std::vector<std::vector<std::string>> tokens = csv_to_tokens(lines);
+    REQUIRE(tokens[0][0] == "this");
+    REQUIRE(tokens[0][1] == "is");
+    REQUIRE(tokens[0][2] == "a");
+    REQUIRE(tokens[0][3] == "test");
+}
+
+TEST_CASE("test_multiline_tokenization", "[fileio]") {
+    std::vector<std::string> lines = file_to_vector("tests/data/longer.txt");
+    std::vector<std::vector<std::string>> tokens = csv_to_tokens(lines);
+    REQUIRE(lines.size() == 4);
+    REQUIRE(tokens[0].size() == 7);
+    REQUIRE(tokens[1].size() == 3);
+    REQUIRE(tokens[2].size() == 1);
+    REQUIRE(tokens[3].size() == 4);
+}
+
