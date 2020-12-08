57a33ff3d999654d180361902d18ff6d538b408d
Justin Kim <justink6@linux-a1.ews.illinois.edu>
2020-12-07 23:14:47 -0600

    added comments for landmark path

--- landmark.h
+++ landmark.h
@@ -8,12 +8,18 @@
 
 class Landmark {
   public: 
+    // Constructor to initialize graph to run landmark algorithm
     Landmark(std::vector<std::vector<std::string>> data);
+
+    // Runs the landmark path algorithm
     std::vector<Edge> runLandmarkPath(Vertex start, Vertex end, Vertex landmark);
 
   private:
     Graph g_;
 
+    // Helper function for runLandmarkPath to find a path given a starting and ending vertex
     std::vector<Edge> findBFSPathToVertex(Vertex start, Vertex end);
+
+    // Helper function for runLandmarkPath to get a vector of edges given a map of previous visited vertices
     std::vector<Edge> getEdgePathFromMap(std::map<Vertex, Vertex> visited);
 }
