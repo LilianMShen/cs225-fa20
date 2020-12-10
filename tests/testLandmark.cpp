4c0455fd81150b4fdb51eb7b918f3fdac1318c4d
Spenser Fong <sfong5@linux-02.ews.illinois.edu>
2020-12-10 03:59:49 -0600

    Landmark constructor tested

--- tests/testLandmark.cpp
+++ tests/testLandmark.cpp
@@ -5,6 +5,30 @@
 #include <vector>
 #include <string>
 
-TEST_CASE("todo", "[landmark]") {
-    REQUIRE(true);
+std::vector<std::vector<std::string>> data = {
+    {"0", "1"},
+    {"0", "2"},
+    {"0", "4"},
+    {"2", "4"},
+    {"2", "5"},
+    {"1", "3"},
+    {"3", "5"},
+    {"3", "7"},
+    {"5", "6"},
+    {"5", "7"}
+};
+Landmark test = Landmark(data);
+Graph graph = test.getGraph();
+
+TEST_CASE("test_landmark_constructor_vertices_edges", "[landmark]") {
+    REQUIRE(graph.edgeExists("0", "1") == true);
+    REQUIRE(graph.edgeExists("0", "2") == true);
+    REQUIRE(graph.edgeExists("0", "4") == true);
+    REQUIRE(graph.edgeExists("2", "4") == true);
+    REQUIRE(graph.edgeExists("2", "5") == true);
+    REQUIRE(graph.edgeExists("1", "3") == true);
+    REQUIRE(graph.edgeExists("3", "5") == true);
+    REQUIRE(graph.edgeExists("3", "7") == true);
+    REQUIRE(graph.edgeExists("5", "6") == true);
+    REQUIRE(graph.edgeExists("5", "7") == true);
 }
